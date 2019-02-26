f8d673a53ccc1edb0be54c094d0d40ede3ee2178
Jalasuthram Durgabhavani <jalasuthram.durgabhavani@capgemini.com>
2019-02-26 15:08:59 +0530

    Durga

--- RBA_Entities/Entity.cs
+++ RBA_Entities/Entity.cs
@@ -14,9 +14,5 @@ namespace RBA_Entities
         public decimal UnitPrice { get; set; }
 
 
-        //public int ProductId { get; set; }
-        //public string ProductName { get; set; }
-        //public string Description { get; set; }
-        //public decimal UnitPrice { get; set; }
     }
 }
